9e59242dbad1c595af7975984c53e6da50e9ad23
kevin Aparicio <aparicio_kevin@hotmail.com>
2024-09-24 18:51:28 -0600

    Manejar entradas no numéricas en el programa

--- verificar_par_impar.py
+++ verificar_par_impar.py
@@ -1,5 +1,3 @@
-#prueba para github actions
-
 def verificar_par_impar(numero):
     if numero % 2 == 0:
         return f"{numero} es un número par."
@@ -7,5 +5,8 @@ def verificar_par_impar(numero):
         return f"{numero} es un número impar."
 
 if __name__ == "__main__":
-    numero = int(input("Introduce un número: "))
-    print(verificar_par_impar(numero))
+    try:
+        numero = int(input("Introduce un número: "))
+        print(verificar_par_impar(numero))
+    except ValueError:
+        print("Error: Debes introducir un número válido.")
